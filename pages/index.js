e900fd27bf1cfb35a3b55cb959ed0bd46dcaee9e
caibingcheng <jack_cbc@163.com>
2021-04-27 23:55:49 +0800

    [Feature] update last update time style
    use UTC

--- pages/index.js
+++ pages/index.js
@@ -97,21 +97,10 @@ export const getStaticProps = async () => {
   })
 
   let date = new Date()
-  let month = date.getMonth() + 1
-  month = month < 10 ? ('0' + month) : ('' + month)
-  let ddate = date.getDate()
-  ddate = ddate < 10 ? ('0' + ddate) : ('' + ddate)
-  let hour = date.getHours()
-  hour = hour < 10 ? ('0' + hour) : ('' + hour)
-  let minute = date.getMinutes()
-  minute = minute < 10 ? ('0' + minute) : ('' + minute)
-  let second = date.getSeconds()
-  second = second < 10 ? ('0' + second) : ('' + second)
-  let updatetime = date.getFullYear() + '-' + month + '-' + ddate + ' ' + hour + ':' + minute + ':' + second
   data = {
     data: data,
     meta: {
-      updatetime: updatetime
+      updatetime: date.toUTCString()
     }
   }
 
