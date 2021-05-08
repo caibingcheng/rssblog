3e7c3f11d7f3d3aa59a9a6d7ff68ad3c37a09e4e
caibingcheng <jack_cbc@163.com>
2021-05-08 21:01:25 +0800

    [Feature] add travelling
    as title

--- components/footer.js
+++ components/footer.js
@@ -2,14 +2,16 @@ import styles from '../styles/Home.module.css'
 
 const Footer = () => (
     <footer className={styles.footer} id="bottom-footer">
-        <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div>
+            <a href="https://travellings.now.sh/" target="_blank" rel="noopener" title="开往-友链接力">
+                <img src="https://travellings.now.sh/assets/logo.gif" alt="开往-友链接力" width="80" />
+            </a>
+        </div>
+        <div>
+            <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+                <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+            </a>
+        </div>
     </footer>
 )
 
