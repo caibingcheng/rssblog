8087f0550a7e475171338900550e6d12d7615d8a
caibingcheng <jack_cbc@163.com>
2021-05-31 17:08:20 +0800

    [RSS] add atom site

--- rss/rss.js
+++ rss/rss.js
@@ -14,6 +14,16 @@ export default {
             link: 'https://www.insidentally.com/atom.xml',
             author: '刘超智',
             method: 'atom'
+        },
+        {
+            link: 'https://www.hin.cool/atom.xml',
+            author: 'W4J1e',
+            method: 'atom'
+        },
+        {
+            link: 'https://7bxing.com/atom.xml',
+            author: 'qinxs',
+            method: 'atom'
         }
     ]
 };
