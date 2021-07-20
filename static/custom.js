f03939099751f71df805912496cd322760b70ec6
caibingcheng <jack_cbc@163.com>
2021-07-20 15:31:10 +0800

    [feat] add baidu analyze

---
+++ static/custom.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4d63ba711285f7c696d0505f5e30ba96";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
