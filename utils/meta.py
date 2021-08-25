ec55cdf38b4221d1d2e1a9b1301a3af9649a4e8f
BBing <jack_cbc@163.com>
2021-08-25 18:30:33 +0800

    [fix] get update time on startup

--- utils/meta.py
+++ utils/meta.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from pytz import timezone
 
+UPTIME = datetime.now(tz=timezone('Asia/Shanghai')).isoformat(timespec='seconds')
 
 def meta():
     mt = {
-        'updatetime': datetime.now(tz=timezone('Asia/Shanghai')).isoformat(timespec='seconds'),
+        'updatetime': UPTIME,
     }
 
     return mt
