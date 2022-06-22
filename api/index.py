b26ec57b92614e9bc024b8971e5dfc579ef02847
caibingcheng <jack_cbc@163.com>
2022-06-22 11:13:54 +0800

    [fix] vercel dir tree

--- api/index.py
+++ api/index.py
@@ -3,6 +3,11 @@ from flaskext.markdown import Markdown
 import time
 import random
 import json
+import os
+import sys
+root_path = os.path.abspath(__file__)
+root_path = '/'.join(root_path.split('/')[:-2])
+sys.path.append(root_path)
 
 from utils.markdown import markdown
 from utils.meta import meta
